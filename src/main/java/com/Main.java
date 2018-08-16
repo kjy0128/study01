7a59623aad87eecde62341db0f01cf7dc4c986eb
kjy0128 <kjy0128@nexon.co.kr>
2018-08-16 14:25:17 +0900

    /매핑 추가

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,6 +1,8 @@
 package com;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by kjy0128 on 2018-08-16.
@@ -10,5 +12,11 @@ public class Main {
         SpringApplication app= new SpringApplication();
         app.run(Main.class, args);
         System.out.println("Spring Boot ST");
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "hello world!";
     }
 }
